f5152397a763221ff5e0880bdb6b8a7a6355c2db
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-10-28 05:01:18 +0700

    create protobuf for schema address

--- src/schema/protobuf/generated/pb/user.pb.go
+++ src/schema/protobuf/generated/pb/user.pb.go
@@ -63,13 +63,20 @@ func (*Empty) Descriptor() ([]byte, []int) {
 	return file_src_schema_protobuf_user_proto_rawDescGZIP(), []int{0}
 }
 
-// as user entity schema
+// as entity user for protobuf schema
 type User struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Id string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	// determine field for message in protobuf with format {data type} {field name} = {unique id and this is dynamic up to you}
+	Id   string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Name string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+	Age  uint64 `protobuf:"varint,3,opt,name=age,proto3" json:"age,omitempty"`
+	// use keyword `repeated` to define array/slice
+	Hobbies []string `protobuf:"bytes,4,rep,name=hobbies,proto3" json:"hobbies,omitempty"`
+	// use custom/object datatype for address user
+	Address []*Address `protobuf:"bytes,5,rep,name=address,proto3" json:"address,omitempty"`
 }
 
 func (x *User) Reset() {
@@ -111,16 +118,103 @@ func (x *User) GetId() string {
 	return ""
 }
 
+func (x *User) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *User) GetAge() uint64 {
+	if x != nil {
+		return x.Age
+	}
+	return 0
+}
+
+func (x *User) GetHobbies() []string {
+	if x != nil {
+		return x.Hobbies
+	}
+	return nil
+}
+
+func (x *User) GetAddress() []*Address {
+	if x != nil {
+		return x.Address
+	}
+	return nil
+}
+
+// use embeded protobuf message inside protobuf message
+type User_Overview struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Intro string `protobuf:"bytes,1,opt,name=intro,proto3" json:"intro,omitempty"`
+}
+
+func (x *User_Overview) Reset() {
+	*x = User_Overview{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_src_schema_protobuf_user_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *User_Overview) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*User_Overview) ProtoMessage() {}
+
+func (x *User_Overview) ProtoReflect() protoreflect.Message {
+	mi := &file_src_schema_protobuf_user_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use User_Overview.ProtoReflect.Descriptor instead.
+func (*User_Overview) Descriptor() ([]byte, []int) {
+	return file_src_schema_protobuf_user_proto_rawDescGZIP(), []int{1, 0}
+}
+
+func (x *User_Overview) GetIntro() string {
+	if x != nil {
+		return x.Intro
+	}
+	return ""
+}
+
 var File_src_schema_protobuf_user_proto protoreflect.FileDescriptor
 
 var file_src_schema_protobuf_user_proto_rawDesc = []byte{
 	0x0a, 0x1e, 0x73, 0x72, 0x63, 0x2f, 0x73, 0x63, 0x68, 0x65, 0x6d, 0x61, 0x2f, 0x70, 0x72, 0x6f,
 	0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x75, 0x73, 0x65, 0x72, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x12, 0x08, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x22, 0x07, 0x0a, 0x05, 0x45, 0x6d,
-	0x70, 0x74, 0x79, 0x22, 0x16, 0x0a, 0x04, 0x55, 0x73, 0x65, 0x72, 0x12, 0x0e, 0x0a, 0x02, 0x69,
-	0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x42, 0x0e, 0x5a, 0x0c, 0x67,
-	0x65, 0x6e, 0x65, 0x72, 0x61, 0x74, 0x65, 0x64, 0x2f, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x33,
+	0x12, 0x08, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x1a, 0x21, 0x73, 0x72, 0x63, 0x2f,
+	0x73, 0x63, 0x68, 0x65, 0x6d, 0x61, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f,
+	0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x07, 0x0a,
+	0x05, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0xa5, 0x01, 0x0a, 0x04, 0x55, 0x73, 0x65, 0x72, 0x12,
+	0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12,
+	0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e,
+	0x61, 0x6d, 0x65, 0x12, 0x10, 0x0a, 0x03, 0x61, 0x67, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x04,
+	0x52, 0x03, 0x61, 0x67, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x68, 0x6f, 0x62, 0x62, 0x69, 0x65, 0x73,
+	0x18, 0x04, 0x20, 0x03, 0x28, 0x09, 0x52, 0x07, 0x68, 0x6f, 0x62, 0x62, 0x69, 0x65, 0x73, 0x12,
+	0x2b, 0x0a, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x18, 0x05, 0x20, 0x03, 0x28, 0x0b,
+	0x32, 0x11, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x41, 0x64, 0x64, 0x72,
+	0x65, 0x73, 0x73, 0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x1a, 0x20, 0x0a, 0x08,
+	0x4f, 0x76, 0x65, 0x72, 0x76, 0x69, 0x65, 0x77, 0x12, 0x14, 0x0a, 0x05, 0x69, 0x6e, 0x74, 0x72,
+	0x6f, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x69, 0x6e, 0x74, 0x72, 0x6f, 0x42, 0x0e,
+	0x5a, 0x0c, 0x67, 0x65, 0x6e, 0x65, 0x72, 0x61, 0x74, 0x65, 0x64, 0x2f, 0x70, 0x62, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -135,17 +229,20 @@ func file_src_schema_protobuf_user_proto_rawDescGZIP() []byte {
 	return file_src_schema_protobuf_user_proto_rawDescData
 }
 
-var file_src_schema_protobuf_user_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_src_schema_protobuf_user_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_src_schema_protobuf_user_proto_goTypes = []interface{}{
-	(*Empty)(nil), // 0: protobuf.Empty
-	(*User)(nil),  // 1: protobuf.User
+	(*Empty)(nil),         // 0: protobuf.Empty
+	(*User)(nil),          // 1: protobuf.User
+	(*User_Overview)(nil), // 2: protobuf.User.Overview
+	(*Address)(nil),       // 3: protobuf.Address
 }
 var file_src_schema_protobuf_user_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	3, // 0: protobuf.User.address:type_name -> protobuf.Address
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_src_schema_protobuf_user_proto_init() }
@@ -153,6 +250,7 @@ func file_src_schema_protobuf_user_proto_init() {
 	if File_src_schema_protobuf_user_proto != nil {
 		return
 	}
+	file_src_schema_protobuf_address_proto_init()
 	if !protoimpl.UnsafeEnabled {
 		file_src_schema_protobuf_user_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Empty); i {
@@ -178,6 +276,18 @@ func file_src_schema_protobuf_user_proto_init() {
 				return nil
 			}
 		}
+		file_src_schema_protobuf_user_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*User_Overview); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -185,7 +295,7 @@ func file_src_schema_protobuf_user_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_src_schema_protobuf_user_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
