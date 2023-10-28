24f7ea72fb3c7f269fd4e8b6f71c5d64e7b2a2ec
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-10-29 02:48:15 +0700

    define psql schema based on protobuf schema

---
+++ src/schema/psqlentity/schema/base.go
@@ -0,0 +1,23 @@
+package schema
+
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+func GetBaseFields() []ent.Field {
+	fields := []ent.Field{
+		field.UUID("id", uuid.UUID{}).Unique().Default(uuid.New),
+		field.Time("created_at").Default(time.Now),
+		field.UUID("created_by", uuid.UUID{}).Unique().Default(uuid.New),
+		field.Time("updated_at").Default(time.Now).Optional(),
+		field.UUID("updated_by", uuid.UUID{}).Unique().Default(uuid.New).Optional(),
+		field.Time("deleted_at").Default(time.Now).Optional(),
+		field.UUID("deleted_by", uuid.UUID{}).Unique().Default(uuid.New).Optional(),
+		field.Int("is_deleted").Default(0).Positive(),
+	}
+	return fields
+}
