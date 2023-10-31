dfc467f706a5a0c6827fbad9b8b2928c09bffd3b
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-11-01 01:27:22 +0700

    added methos to get list sale with pagination

--- src/business/usecase/sale/sale.go
+++ src/business/usecase/sale/sale.go
@@ -6,7 +6,10 @@ import (
 	"github.com/irdaislakhuafa/learn-grpc-go/src/schema/entity"
 	"github.com/irdaislakhuafa/learn-grpc-go/src/schema/parameter"
 	"github.com/irdaislakhuafa/learn-grpc-go/src/schema/psqlentity/schema/generated"
+	psqlSale "github.com/irdaislakhuafa/learn-grpc-go/src/schema/psqlentity/schema/generated/sale"
 	"github.com/irdaislakhuafa/learn-grpc-go/src/utils/config"
+	"github.com/irdaislakhuafa/learn-grpc-go/src/utils/operator"
+	"github.com/irdaislakhuafa/learn-grpc-go/src/utils/pagination"
 )
 
 type Interface interface {
@@ -31,7 +34,56 @@ func Init(psql *generated.Client, cfg config.Config) Interface {
 }
 
 func (self *sale) GetListWithPagination(ctx context.Context, params parameter.PaginationParam) (*entity.ResponsePagination[entity.Pagination, []entity.Sale], error) {
-	panic("not implemented") // TODO: Implement
+	params, offset, err := pagination.ParseFromParam(params)
+	if err != nil {
+		return nil, err
+	}
+
+	listSale, err := self.psql.Sale.Query().
+		Limit(int(params.Limit)).
+		Offset(int(offset)).
+		Where(psqlSale.IsDeleted(params.IsDeleted)).
+		All(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	totalSale, err := self.psql.Sale.Query().Where(psqlSale.IsDeleted(params.IsDeleted)).Count(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	listSaleEntity := []entity.Sale{}
+	for _, v := range listSale {
+		sale := entity.Sale{
+			ID:          v.ID,
+			ProductID:   v.ProductID,
+			Quantity:    v.Quantity,
+			TotalAmount: v.TotalAmount,
+			Date:        v.Date,
+			CreatedAt:   v.CreatedAt,
+			CreatedBy:   v.CreatedBy,
+			UpdatedAt:   v.UpdatedAt,
+			UpdatedBy:   v.UpdatedBy,
+			DeletedAt:   v.DeletedAt,
+			DeletedBy:   v.DeletedBy,
+			IsDeleted:   v.IsDeleted,
+		}
+		listSaleEntity = append(listSaleEntity, sale)
+	}
+
+	totalPages := uint64(totalSale / int(params.Limit))
+	result := entity.ResponsePagination[entity.Pagination, []entity.Sale]{
+		Pagination: entity.Pagination{
+			Total:       uint64(totalSale),
+			Limit:       params.Limit,
+			CurrentPage: params.Page,
+			TotalPages:  operator.Ternary[uint64](totalPages <= 0, 1, totalPages),
+		},
+		Data: listSaleEntity,
+	}
+
+	return &result, nil
 }
 
 func (self *sale) Get(ctx context.Context, params parameter.SaleGetParam) (*entity.Sale, error) {
